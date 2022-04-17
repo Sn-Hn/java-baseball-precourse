6d31b940fc483aee4cc4105549097459b7e8603d
ShinHan <tlsgks8668@naver.com>
2022-04-17 18:59:27 +0900

    refactor: 검증 로직 접근제어자 수정

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -25,25 +25,25 @@ public class UserNumber implements Number {
     return new ArrayList<>(userNumbers);
   }
 
-  public void validate() {
+  private void validate() {
     validateEscape(inputNumber);
     validateDigit(inputNumber);
     validateDuplication(inputNumber);
   }
 
-  public void validateEscape(String inputNumber) {
+  private void validateEscape(String inputNumber) {
     if (!numberPattern.matcher(inputNumber).find()) {
       throw new IllegalArgumentException("1에서 9까지의 숫자만 입력할 수 있습니다.");
     }
   }
 
-  public void validateDigit(String inputNumber) {
+  private void validateDigit(String inputNumber) {
     if (inputNumber.length() != MAX_NUMBER_LENGTH) {
       throw new IllegalArgumentException("세 자리의 숫자만 입력할 수 있습니다.");
     }
   }
 
-  public void validateDuplication(String inputNumber) {
+  private void validateDuplication(String inputNumber) {
     Set<String> convertInputNumber = new LinkedHashSet<>(Arrays.asList(inputNumber.split("")));
     if (convertInputNumber.size() != MAX_NUMBER_LENGTH) {
       throw new IllegalArgumentException("중복되지 않은 숫자만 입력할 수 있습니다.");
