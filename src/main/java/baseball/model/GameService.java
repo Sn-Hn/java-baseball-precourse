801cdbf1b396b6d75b60e37b7e0a9f18de184624
ShinHan <tlsgks8668@naver.com>
2022-04-17 19:05:53 +0900

    feat: 게임 종료 후 잘못된 값 검증
    - 1, 2를 제외한 값을 입력하면 IllegalArgumentException을 발생 후 종료

--- src/main/java/baseball/model/GameService.java
+++ src/main/java/baseball/model/GameService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class GameService {
   private final String RESTART_INPUT = "1";
+  private final String RESTART_END = "2";
 
   public PlayResult startGame(Balls computerBalls, List<Integer> userNumber) {
 
@@ -12,10 +13,17 @@ public class GameService {
   }
 
   public GameStatus restartOrEndGame(String restartInput) {
+    validate(restartInput);
     if (RESTART_INPUT.equals(restartInput)) {
       return GameStatus.RESTART;
     }
 
     return GameStatus.END;
+  }
+
+  public void validate(String input) {
+    if (!RESTART_INPUT.equals(input) && !RESTART_END.equals(input)) {
+      throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+    }
   }
 }
